3c803a633a9e9d39404da4238636c389e6c98ae8
dimje <mark759@naver.com>
2024-05-01 18:03:42 +0900

    포켓몬 상세 페이지 스탯 bar 구현 및 SharedTransitionLayout 미작동 이슈 수정

--- app/src/main/java/com/example/pokepoke/MainActivity.kt
+++ app/src/main/java/com/example/pokepoke/MainActivity.kt
@@ -1,36 +1,23 @@
 package com.example.pokepoke
 
 import android.annotation.SuppressLint
-import android.graphics.drawable.shapes.OvalShape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.AnimatedVisibilityScope
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.SharedTransitionScope
-import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -39,7 +26,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -51,12 +37,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -65,18 +49,12 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
@@ -86,34 +64,30 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import androidx.palette.graphics.Palette
 import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
-import com.example.pokepoke.data.Pokemon
-import com.example.pokepoke.data.PokemonDetail
 import com.example.pokepoke.data.PokemonListItem
 import com.example.pokepoke.data.ScreenState
-import com.example.pokepoke.network.ApiService
+import com.example.pokepoke.data.toStatData
 import com.example.pokepoke.ui.theme.PokePokeTheme
 import com.example.pokepoke.ui.theme.background
 import com.example.pokepoke.ui.theme.colorMap
 import com.example.pokepoke.viewmodel.PokeViewModel
-import com.skydoves.landscapist.CircularReveal
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.components.rememberImageComponent
 import com.skydoves.landscapist.glide.GlideImage
-import com.skydoves.landscapist.palette.BitmapPalette
+import com.skydoves.landscapist.palette.PalettePlugin
+import com.skydoves.landscapist.palette.rememberPaletteState
 import dagger.hilt.android.AndroidEntryPoint
-
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -141,26 +115,12 @@ class MainActivity : ComponentActivity() {
                                 PokemonLazyVerticalGrid(this@composable)
                             }
                             composable(
-                                route = "${ScreenState.Detail.name}/{color}",
-                                arguments = listOf(navArgument("color"){
-                                    type = NavType.IntType
-                                })
+                                route = "${ScreenState.Detail.name}/{color}/{index}",
+                                arguments = listOf(navArgument("color"){ type = NavType.IntType }, navArgument("index"){type = NavType.IntType})
                             ) {
-                                DetailView(it.arguments?.getInt("color",255)!!,this@composable)
+                                DetailView(it.arguments?.getInt("color",255)!!,it.arguments?.getInt("index",0)!!,this@composable)
                             }
                         }
-//                        AnimatedContent(targetState = screenState, label = "", contentKey = {it.javaClass},transitionSpec = {
-//                            if (initialState == ScreenState.Main) {
-//                                slideInHorizontally { -it } + fadeIn() togetherWith slideOutHorizontally { it } + fadeOut()
-//                            } else {
-//                                slideInHorizontally { it } + fadeIn() togetherWith slideOutHorizontally { -it } + fadeOut()
-//                            }
-//                        }) {
-//                            when (it) {
-//                                ScreenState.Main -> PokemonLazyVerticalGrid(this)
-//                                ScreenState.Detail -> DetailView(this)
-//                            }
-//                        }
                     }
                 }
             }
@@ -196,30 +156,28 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun SharedTransitionScope.ListItem(data: PokemonListItem,animatedContentScope: AnimatedVisibilityScope){
 
-        var palette by remember {
-            mutableStateOf<Palette?>(null)
-        }
+        var palette by rememberPaletteState(value = null)
 
         Column(modifier = Modifier
-            .fillMaxWidth(0.5f)
             .padding(10.dp)
             .combinedClickable(
                 onClick = {
                     viewModel.loadDetail(data.number.toString())
-                    //viewModel.navToDetail()
-                    navController.navigate("${ScreenState.Detail.name}/${palette?.dominantSwatch?.rgb}")
+                    navController.navigate("${ScreenState.Detail.name}/${palette?.dominantSwatch?.rgb}/${data.number}")
 
                 },
             )
         ) {
-            GlideImage(imageModel = data.imgUrl,
-                requestOptions = { RequestOptions().encodeQuality(30).format(DecodeFormat.PREFER_RGB_565).centerCrop()
+            GlideImage(imageModel = {data.imgUrl},
+                requestOptions = {RequestOptions().encodeQuality(30).format(DecodeFormat.PREFER_RGB_565).diskCacheStrategy(DiskCacheStrategy.ALL)
                 },
-                contentDescription = "test",
-                bitmapPalette = BitmapPalette{
-                    palette = it
+                imageOptions = ImageOptions(
+                    contentScale = ContentScale.Crop,
+                    alignment = Alignment.Center
+                ),
+                component = rememberImageComponent {
+                    +PalettePlugin{ palette = it}
                 },
-                contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentHeight()
@@ -228,9 +186,8 @@ class MainActivity : ComponentActivity() {
                         color = Color(palette?.dominantSwatch?.rgb ?: 255),
                         shape = RectangleShape
                     )
-
                     .sharedElement(
-                        rememberSharedContentState(key = data.name),
+                        rememberSharedContentState(key = "image-${data.number}"),
                         animatedVisibilityScope = animatedContentScope
                     )
 
@@ -240,10 +197,6 @@ class MainActivity : ComponentActivity() {
                 color = Color.Gray,
                 modifier = Modifier
                     .padding(top = 5.dp)
-//                    .then(Modifier.sharedElement(
-//                        rememberSharedContentState(key = "number${data.number}"),
-//                        animatedVisibilityScope = animatedContentScope
-//                    ).skipToLookaheadSize())
             )
             Text(
                 text = data.name.replaceFirstChar{it.uppercase()},
@@ -258,55 +211,56 @@ class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalSharedTransitionApi::class)
     @Composable
-    fun SharedTransitionScope.DetailView(color:Int,animatedContentScope: AnimatedVisibilityScope){
+    fun SharedTransitionScope.DetailView(color:Int,index:Int,animatedContentScope: AnimatedVisibilityScope){
 
 
         val pokemonDetail by remember { viewModel.pokemonDetail }
-        var expanded by remember { mutableStateOf(false) }
         Column(modifier = Modifier
             .fillMaxSize()
             .background(background)) {
-            pokemonDetail?.let {
-                val imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${it.id}.png"
-                Box(modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.35f)
-                    .background(
-                        color = background,
-                        shape = RectangleShape
-                    )
-                ) {
-
-                    GlideImage(imageModel = imgUrl,
-                        requestOptions = { RequestOptions().encodeQuality(30).format(DecodeFormat.PREFER_RGB_565).centerCrop()
-                        },
-                        contentDescription = "test",
+            val imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index}.png"
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.35f)
+                .background(
+                    color = background,
+                    shape = RectangleShape
+                )
+            ) {
+                //기존엔 viewmodel에서 상세 정보 조회 후 이미지가 그려졌기에 가끔 sharedTransition이 작동하지 않았다고 생각함
+                //
+                GlideImage(imageModel = { imgUrl },
+                    imageOptions = ImageOptions(
                         contentScale = ContentScale.Fit,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight()
-                            .clip(RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp))
-                            .background(
-                                color = Color(color),
-                                shape = RectangleShape
-                            )
-                            .sharedElement(
-                                rememberSharedContentState(key = it.name),
-                                animatedVisibilityScope = animatedContentScope
-                            )
+                        alignment = Alignment.Center
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .clip(RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp))
+                        .background(
+                            color = Color(color),
+                            shape = RectangleShape
+                        )
+                        .sharedElement(
+                            rememberSharedContentState(key = "image-${index}"),
+                            animatedVisibilityScope = animatedContentScope
+                        )
 
-                    )
-                    IconButton(modifier = Modifier.align(Alignment.TopStart),onClick = {
-                        navController.navigateUp()
-                        viewModel.pokemonDetail.value = null
-                    }) {
-                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back", tint = Color.White)
+                )
+                IconButton(modifier = Modifier.align(Alignment.TopStart),onClick = {
+                    navController.navigateUp()
+                    viewModel.pokemonDetail.value = null
+                }) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back", tint = Color.White)
 
-                    }
-                    Text(text = String.format("No.%04d",it.id), color = Color.White, fontWeight = FontWeight.Bold, modifier = Modifier
-                        .align(Alignment.TopEnd)
-                        .padding(top = 10.dp, bottom = 10.dp, end = 7.dp))
                 }
+                Text(text = String.format("No.%04d",index), color = Color.White, fontWeight = FontWeight.Bold, modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(top = 10.dp, bottom = 10.dp, end = 7.dp))
+            }
+
+            pokemonDetail?.let {
 
                 Text(text = it.name, color = Color.White, fontWeight = FontWeight.Bold, fontSize = 27.sp, modifier = Modifier
                     .padding(top = 20.dp, bottom = 10.dp)
@@ -327,11 +281,13 @@ class MainActivity : ComponentActivity() {
                     Physical(type = "Weight", data = it.weight)
                     Physical(type = "Height", data = it.height)
                 }
-                Column(verticalArrangement = Arrangement.Center) {
-                    it.stats.forEach {stats ->
-                        Row(horizontalArrangement = Arrangement.SpaceEvenly) {
-                            Text(modifier = Modifier.fillMaxWidth(0.2f).padding(start = 10.dp, end = 10.dp),text = stats.stat.name, textAlign = TextAlign.Center)
-                            PokeProgressBar(typeName = stats.stat.name, baseStat = stats.baseStat)
+                Column {
+                    it.toStatData().forEach {stats ->
+                        Row(horizontalArrangement = Arrangement.SpaceEvenly, verticalAlignment = Alignment.CenterVertically) {
+                            Text(modifier = Modifier
+                                .fillMaxWidth(0.2f)
+                                .padding(start = 10.dp),text = stats.statsName, color = Color.White, fontWeight = FontWeight.Bold, textAlign = TextAlign.Start)
+                            PokeProgressBar(baseStat = stats.stats, barColor = stats.color)
                         }
                         //PokeProgressBar(typeName = stats.stat.name, baseStat = stats.baseStat)
                         Spacer(modifier = Modifier.height(15.dp))
@@ -395,7 +351,7 @@ fun TypeBox(text:String){
     }
 }
 @Composable
-fun PokeProgressBar(typeName: String,baseStat: Int){
+fun PokeProgressBar(baseStat: Int,barColor: Color){
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp.value
     val isLocalInspectionMode = LocalInspectionMode.current
     var progressWidth by remember {
@@ -409,16 +365,17 @@ fun PokeProgressBar(typeName: String,baseStat: Int){
     }
     Box(
         modifier = Modifier
-            .padding(horizontal = 16.dp)
+            .padding(end = 20.dp)
             .fillMaxWidth()
             .height(18.dp)
-            .onSizeChanged { progressWidth = it.width * 0.25f }
+            .onSizeChanged { progressWidth = it.width * (baseStat / 300f) }
             .background(
                 color = Color.White,
                 shape = RoundedCornerShape(64.dp),
             )
             .clip(RoundedCornerShape(64.dp))
         ,
+        contentAlignment = Alignment.Center
     ) {
         var textWidth by remember { mutableIntStateOf(0) }
         val threshold = 16
@@ -446,7 +403,7 @@ fun PokeProgressBar(typeName: String,baseStat: Int){
                 .height(18.dp)
                 .padding(end = 10.dp)
                 .background(
-                    color = Color.Gray,
+                    color = barColor,
                     shape = RoundedCornerShape(64.dp),
                 ),
             contentAlignment = Alignment.Center
@@ -457,10 +414,11 @@ fun PokeProgressBar(typeName: String,baseStat: Int){
                         .onSizeChanged { textWidth = it.width }
                         .align(Alignment.CenterEnd)
                         .padding(end = (threshold * 2).pxToDp()),
-                    text = "150/300",
+                    text = "$baseStat/300",
                     fontSize = 12.sp,
                     color = Color.White,
-                    textAlign = TextAlign.Center
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
                 )
             }
         }
@@ -468,6 +426,7 @@ fun PokeProgressBar(typeName: String,baseStat: Int){
         if (!isInner) {
             Text(
                 modifier = Modifier
+                    .fillMaxHeight()
                     .onSizeChanged { textWidth = it.width }
                     .align(Alignment.CenterStart)
                     .padding(
@@ -475,10 +434,11 @@ fun PokeProgressBar(typeName: String,baseStat: Int){
                             .toInt()
                             .pxToDp() + threshold.pxToDp(),
                     ),
-                text = "150/300",
+                text = "$baseStat/300",
                 fontSize = 12.sp,
                 color = Color.Black,
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
             )
         }
 
